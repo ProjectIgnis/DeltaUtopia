93787ae8db9dcaa7619d90e770fcd028eb243c67
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-01-29 00:50:31 +0000

    Fix for "Ostinato"
    Should be able to activate Ghost Belle in response to it as it SSs from GY.

---
+++ script/official/c9113513.lua
@@ -0,0 +1,64 @@
+--オスティナート
+--Ostinato
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0x9b),Fusion.InHandMat,s.fextra,nil,nil,s.stage2,2)
+	e1:SetCondition(s.condition)
+	c:RegisterEffect(e1)
+	if not AshBlossomTable then AshBlossomTable={} end
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(AshBlossomTable,e1)
+	table.insert(GhostBelleTable,e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_DECK,0,nil)
+end
+function s.stage2(e,tc,tp,mg,chk)
+	if chk==0 then
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	end
+	if chk==1 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetLabelObject(tc)
+		e1:SetCondition(s.descon)
+		e1:SetOperation(s.desop)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:GetFlagEffect(id)~=0 then
+		return true
+	else
+		e:Reset()
+		return false
+	end
+end
+function s.mgfilter(c,e,tp,fusc,mg)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_GRAVE)
+		and (c:GetReason()&0x40008)==0x40008 and c:GetReasonCard()==fusc
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and fusc:CheckFusionMaterial(mg,c)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	local mg=tc:GetMaterial()
+	local sumtype=tc:GetSummonType()
+	if Duel.Destroy(tc,REASON_EFFECT)~=0
+		and (sumtype&SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION and #mg>0
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>=#mg
+		and mg:IsExists(aux.NecroValleyFilter(s.mgfilter),#mg,nil,e,tp,tc,mg)
+		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.SpecialSummon(mg,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
