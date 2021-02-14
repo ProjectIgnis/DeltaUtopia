92309355dd1ef82876856fb988087c01ffa09a9a
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-02-14 22:34:34 +0000

    Update "Over Tuning"
    Overhauled script to allow correct implementation.

---
+++ script/unofficial/c511001606.lua
@@ -0,0 +1,64 @@
+--オーバー・チューニング
+--Over Tuning
+Duel.LoadScript("c420.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	aux.GlobalCheck(s,function()
+        	s[0]=false
+        	s[1]=false
+        	local ge1=Effect.CreateEffect(c)
+        	ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        	ge1:SetCode(EVENT_SUMMON_SUCCESS)
+        	ge1:SetOperation(s.checkop)
+        	Duel.RegisterEffect(ge1,0)
+        	local ge2=ge1:Clone()
+        	ge2:SetCode(EVENT_SPSUMMON_SUCCESS)
+        	Duel.RegisterEffect(ge2,0)
+        	local ge3=ge1:Clone()
+        	ge3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+        	Duel.RegisterEffect(ge3,0)
+        	local ge4=Effect.CreateEffect(c)
+        	ge4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        	ge4:SetCode(EVENT_ADJUST)
+        	ge4:SetCountLimit(1)
+        	ge4:SetOperation(s.clear)
+        	Duel.RegisterEffect(ge4,0)
+    	end)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=eg:GetFirst()
+    while tc do
+        if tc:IsType(TYPE_TUNER) then
+            s[tc:GetSummonPlayer()]=true
+        end
+        tc=eg:GetNext()
+    end
+end
+function s.clear(e,tp,eg,ep,ev,re,r,rp)
+    s[0]=false
+    s[1]=false
+end
+function s.filter(c,e,tp)
+    return c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        end
+    end
+end
