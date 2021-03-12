f5ee0f5d45d997ba5e99b5b888a78b7e8609e150
Edoardo Lolletti <edoardo762@gmail.com>
2021-03-12 18:49:28 +0000

    Update Reprodocus
    Fixed its attribute/race declaration part

---
+++ script/official/c34989413.lua
@@ -0,0 +1,47 @@
+--リプロドクス
+--Reprodocus
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,nil,2,2)
+	--change property
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then	return e:GetHandler():GetLinkedGroup():FilterCount(aux.FilterFaceupFunction(Card.IsLocation,LOCATION_MZONE),nil)>0 end
+	e:SetLabel(Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2)))
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local lg=c:GetLinkedGroup():Filter(aux.FilterFaceupFunction(Card.IsLocation,LOCATION_MZONE),nil)
+	if #lg==0 then return end
+	local eff=0
+	local val=0
+	if e:GetLabel()==0 then
+		eff=EFFECT_CHANGE_RACE
+		local race=0
+		lg:ForEach(function(c)race=race|c:GetRace() end)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RACE)
+		val=Duel.AnnounceRace(tp,1,race&(race-1)==0 and ~race or 0xff)
+	else
+		eff=EFFECT_CHANGE_ATTRIBUTE
+		val=aux.AnnounceAnotherAttribute(lg,tp)
+	end
+	for tc in aux.Next(lg) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(eff)
+		e1:SetValue(val)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
