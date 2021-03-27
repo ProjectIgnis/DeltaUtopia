82842f7d7717eff61354aee033b59b5c91f3df3e
Edoardo Lolletti <edoardo762@gmail.com>
2021-03-27 15:56:33 +0000

    Fix Sunvine Cross Breed

--- script/unofficial/c511009938.lua
+++ script/unofficial/c511009938.lua
@@ -1,12 +1,11 @@
---聖蔓の交配
---Sunvine Cross Breed
---Scripted by Eerie Code, based on the anime version
+--Sunvine Cross Breed (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
 	--special summon
 	local e2=Effect.CreateEffect(c)
@@ -15,42 +14,24 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCountLimit(1,id)
 	e2:SetCost(s.spcost)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsType,1,false,aux.ChkfMMZ(1),nil,TYPE_LINK) end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsType,1,1,false,aux.ChkfMMZ(1),nil,TYPE_LINK)
+	Duel.Release(g,REASON_COST)
+end
 function s.spfilter(c,e,tp)
 	return c:IsRace(RACE_PLANT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	e:SetLabel(1)
-	return true
-end
-function s.zonecheck(sg,tp,exg)
-	return sg:FilterCount(Auxiliary.MZFilter,nil,tp)+Duel.GetLocationCount(tp,LOCATION_MZONE)>=1
-end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) 
-		and s.spfilter(chkc,e,tp) and chkc~=e:GetLabelObject() end
-	if chk==0 then 
-		if e:GetLabel()==1 then
-			e:SetLabel(0)
-			return Duel.CheckReleaseGroupCost(tp,Card.IsType,1,false,s.zonecheck,nil,TYPE_LINK)
-		else
-			return Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
-		end
-	end
-	local rg=nil
-	if e:GetLabel()==1 then
-		e:SetLabel(0)
-		rg=Duel.SelectReleaseGroupCost(tp,Card.IsType,1,1,false,s.zonecheck,nil,TYPE_LINK)
-		Duel.Release(rg,REASON_COST)
-		e:SetLabelObject(rg)
-	end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,rg,e,tp)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
