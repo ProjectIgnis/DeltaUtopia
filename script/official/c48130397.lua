9e2420814ad3e58e94e4d1b653473d4418b215b7
Edoardo Lolletti <edoardo762@gmail.com>
2021-04-02 17:31:08 +0000

    Minor update to super polymerization and rapid trigger

---
+++ script/official/c48130397.lua
@@ -0,0 +1,22 @@
+--超融合
+--Super Polymerization
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff{handler=c,matfilter=Fusion.OnFieldMat,extrafil=s.fextra,extratg=s.extratg}
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetCost(s.cost)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup),tp,0,LOCATION_ONFIELD,nil)
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+		Duel.SetChainLimit(aux.FALSE)
+	end
+end
