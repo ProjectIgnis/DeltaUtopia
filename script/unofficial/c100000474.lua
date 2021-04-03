4f7d84dd4b5c2cde54226db19eac26d920ebab7f
Edoardo Lolletti <edoardo762@gmail.com>
2021-04-03 13:02:48 +0000

    Rewrite Fusion Birth
    Clean up its script and properly implement it

---
+++ script/unofficial/c100000474.lua
@@ -0,0 +1,33 @@
+--フュージョン・バース
+--Fusion Birth
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,5) end
+	Duel.DiscardDeck(tp,5,REASON_COST)
+	local g=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_GRAVE)
+	Duel.SetTargetCard(g)
+end
+function s.filter(c,e,tp,m,chkf)
+	return c:IsType(TYPE_FUSION) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+		and c:CheckFusionMaterial(m,nil,chkf)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local chkf=tp|FUSPROC_NOTFUSION
+	local g=Duel.GetTargetCards(e):Filter(Card.IsCanBeFusionMaterial,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,g,chkf):GetFirst()
+	if tc then
+		Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+		tc:CompleteProcedure()
+	end
+end
