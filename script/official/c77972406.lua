cfe1d15a7fc67695978489969288afb4142a5230
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-04-29 01:21:51 +0000

    Fix for official cards that end the Battle Phase
    Now they properly proceed the game to the End Step rather than skipping the rest of the Battle Phase, so cards like "Evenly Matched" can be used.

---
+++ script/official/c77972406.lua
@@ -0,0 +1,38 @@
+--反撃の毒牙
+--Ambush Fangs
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x50}
+s.counter_place_list={0x1009}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttackTarget()
+	return tc and tc:IsControler(tp) and tc:IsFaceup() and tc:IsSetCard(0x50)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local tg=Duel.GetAttacker()
+	if chkc then return chkc==tg end
+	if chk==0 then return tg:IsOnField() and tg:IsCanBeEffectTarget(e) end
+	Duel.SetTargetCard(tg)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttacker()
+	if tc:IsRelateToEffect(e) and Duel.NegateAttack() then
+		Duel.SkipPhase(1-tp,PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE_STEP,1)
+		Duel.BreakEffect()
+		local atk=tc:GetAttack()
+		tc:AddCounter(0x1009,1)
+		if atk>0 and tc:GetAttack()==0 then
+			Duel.RaiseEvent(tc,EVENT_CUSTOM+54306223,e,0,0,0,0)
+		end
+	end
+end
