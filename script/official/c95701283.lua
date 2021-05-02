924c5f3f2d56001580b15e52b9d76809651a3799
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-02 01:01:02 +0000

    "Majestic Mech - Goryu" fix
    The effect to send itself to the GY should not be activated if the effect of "Kaiser Sea Horse" is applied for its Tribute Summon.

---
+++ script/official/c95701283.lua
@@ -0,0 +1,36 @@
+--光神機－轟龍
+--Majestic Mech - Goryu
+local s,id=GetID()
+function s.initial_effect(c)
+	--Tribute Summon with 1 Tribute
+	local e1=aux.AddNormalSummonProcedure(c,true,true,1,1,SUMMON_TYPE_TRIBUTE,aux.Stringid(id,0),nil,s.sumop)
+	--Piercing
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_PIERCE)
+	c:RegisterEffect(e2)
+end
+function s.sumop(g,e,tp,eg,ep,ev,re,r,rp,c,minc,zone,relzone,exeff)
+	--Send to GY during the End Phase
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.tgtg)
+	e1:SetOperation(s.tgop)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+	c:RegisterEffect(e1)
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,e:GetHandler(),1,0,0)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.SendtoGrave(c,REASON_EFFECT)
+	end
+end
