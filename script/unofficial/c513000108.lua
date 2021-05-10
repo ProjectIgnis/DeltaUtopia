28726fc6fe042187e15e09a46a674a38f391dd89
Larry126 <28985622+larry126@users.noreply.github.com>
2021-05-10 01:34:48 +0000

    "Infernity Des Gunman" clean-up

---
+++ script/unofficial/c513000108.lua
@@ -0,0 +1,103 @@
+--インフェルニティ・デス・ガンマン (Anime)
+--Infernity Des Gunman (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Negate Damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetProperty(CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCondition(s.condition)
+	e1:SetCost(aux.bfgcost)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	aux.GlobalCheck(s,function()
+		s[0]=0
+		s[1]=0
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_DAMAGE)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+		local ge2=Effect.CreateEffect(c)
+		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge2:SetCode(EVENT_ADJUST)
+		ge2:SetCountLimit(1)
+		ge2:SetOperation(s.clear)
+		Duel.RegisterEffect(ge2,0)
+	end)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if (r&REASON_EFFECT)>0 then
+		s[ep]=s[ep]+ev
+	end
+end
+function s.clear(e,tp,eg,ep,ev,re,r,rp)
+	s[0]=0
+	s[1]=0
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0 then return false end
+	local e1=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_DAMAGE)
+	local e2=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_RECOVER)
+	local rd=e1 and not e2
+	local rr=not e1 and e2
+	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
+	if ex and (cp==tp or cp==PLAYER_ALL) and not rd and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) then 
+		e:SetLabel(cv)
+		return true 
+	end
+	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
+	if ex and (cp==tp or cp==PLAYER_ALL) and rr and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) then
+		e:SetLabel(cv)
+		return true
+	end
+	return false
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetLabel(Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID))
+	e1:SetValue(s.refcon)
+	e1:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(e1,tp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 and s[tp]>0 and Duel.SelectEffectYesNo(1-tp,c) then
+		Duel.ConfirmDecktop(tp,1)
+		local tc=Duel.GetDecktopGroup(tp,1):GetFirst()
+		if tc:IsType(TYPE_MONSTER) then
+			Duel.Damage(1-tp,s[tp]+e:GetLabel(),REASON_EFFECT)
+		else
+			Duel.Damage(tp,s[tp],REASON_EFFECT)
+		end
+	else
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_CHANGE_DAMAGE)
+		e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e2:SetTargetRange(1,0)
+		e2:SetValue(s.damval)
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e2,tp)
+		local e3=e2:Clone()
+		e3:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+		e3:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e3,tp)
+	end
+end
+function s.refcon(e,re,val,r,rp,rc)
+	local cc=Duel.GetCurrentChain()
+	if cc==0 or (r&REASON_EFFECT)==0 then return end
+	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
+	if cid==e:GetLabel() then return 0
+	else return val end
+end
+function s.damval(e,re,val,r,rp,rc)
+	if (r&REASON_EFFECT)~=0 then return 0
+	else return val end
+end
