e0aad4b789454ef8f5ec37bdc700461d895a1980
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-13 18:11:37 +0000

    Updated old cards to use the new "Kuriboh" constant

---
+++ script/official/c89086566.lua
@@ -0,0 +1,36 @@
+--機雷化
+--Detonate
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_KURIBOH}
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsCode(CARD_KURIBOH,40703223)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,0,1,nil) end
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_ONFIELD,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_ONFIELD,0,nil)
+	local dt=Duel.Destroy(g,REASON_EFFECT)
+	if dt==0 then return end
+	local dg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
+	if #dg>0 then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local sg=dg:Select(tp,1,dt,nil)
+		Duel.HintSelection(sg)
+		Duel.Destroy(sg,REASON_EFFECT)
+	end
+end
---
+++ script/unofficial/c511000690.lua
@@ -0,0 +1,36 @@
+--虹色の祝福
+--Rainbow Blessing (DM)
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsCode,CARD_KURIBOH))
+	--Direct
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.dirtar)
+	e2:SetOperation(s.diract)
+	c:RegisterEffect(e2)
+end
+s.listed_names={CARD_KURIBOH}
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local eq=e:GetHandler():GetEquipTarget()
+	if chk==0 then return eq:IsReleasable() end
+	Duel.Release(eq,REASON_COST)
+end
+function s.dirtar(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.diract(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DIRECT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		tc=g:GetNext()
+	end
+end
