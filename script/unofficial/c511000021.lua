7336e6e60d9ac1acdc79e3af8da989b4602506cb
Larry126 <28985622+larry126@users.noreply.github.com>
2021-05-21 08:50:33 +0000

    "Burst Impact" clean-up

---
+++ script/unofficial/c511000021.lua
@@ -0,0 +1,40 @@
+--バースト・インパクト
+--Burst Impact
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={58932615}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,58932615),tp,LOCATION_ONFIELD,0,1,nil)
+end
+function s.filter(c)
+	return not c:IsCode(58932615) or c:IsFacedown()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,0,#g*300)
+end
+function s.cfilter(c,p)
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(p)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local dg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.Destroy(dg,REASON_EFFECT)
+	local og=Duel.GetOperatedGroup()
+	local ct1=og:FilterCount(s.cfilter,nil,tp)
+	local ct2=og:FilterCount(s.cfilter,nil,1-tp)
+	Duel.Damage(tp,ct1*300,REASON_EFFECT,true)
+	Duel.Damage(1-tp,ct2*300,REASON_EFFECT,true)
+	Duel.RDComplete()
+end
