30617985dcebf262328ced8d8451c597815618ce
Larry126 <28985622+larry126@users.noreply.github.com>
2021-05-21 04:27:21 +0000

    Updated AC01-related unofficial cards

---
+++ script/unofficial/c500000143.lua
@@ -0,0 +1,40 @@
+--ケンタウルミナ (VG)
+--Centaur Mina (VG)
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,100278019,100278018)
+	--negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.discon)
+	e1:SetTarget(s.distg)
+	e1:SetOperation(s.disop)
+	c:RegisterEffect(e1)
+end
+s.listed_names={100278018,100278019}
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsActiveType(TYPE_TRAP) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
+		and rp~=tp and tp==Duel.GetTurnPlayer()
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return re:GetHandler():IsCanTurnSet() end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateActivation(ev)
+	local rc=re:GetHandler()
+	if rc:IsRelateToEffect(re) and rc:IsCanTurnSet() then
+		rc:CancelToGrave()
+		Duel.ChangePosition(rc,POS_FACEDOWN)
+		rc:SetStatus(STATUS_ACTIVATE_DISABLED,false)
+		Duel.RaiseEvent(rc,EVENT_SSET,e,REASON_EFFECT,tp,tp,0)
+	end
+end
