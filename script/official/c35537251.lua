eb0445fe8821a91a9076079e8fc92c61d23ca725
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-24 18:19:40 +0000

    "Battlin' Boxer Shadow" fix
    Was executing a loop incorrectly causing it to skip some of your "Battlin' Boxer" Xyz Monsters in the activation check. Partially re-wrote the effect to make it clearer for the players from which Xyz Monster the material is being detached from, fixing the aforementioned issue in the process.

---
+++ script/official/c35537251.lua
@@ -0,0 +1,43 @@
+--ＢＫ シャドー
+--Battlin' Boxer Shadow
+local s,id=GetID()
+function s.initial_effect(c)
+	--Detach material and Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x84}
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x84) and c:IsType(TYPE_XYZ)
+		and c:GetOverlayCount()>0
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DEATTACHFROM)
+	local xyzg=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local tc=xyzg:GetFirst()
+	if not tc then return end
+	Duel.HintSelection(xyzg)
+	local mg=tc:GetOverlayGroup()
+	local ct=#mg
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVEXYZ)
+	local sg=mg:Select(tp,1,1,nil)
+	if #sg>0 and Duel.SendtoGrave(sg,REASON_EFFECT)>0
+		and tc:GetOverlayCount()<ct then
+		local c=e:GetHandler()
+		if not c:IsRelateToEffect(e) then return end
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
