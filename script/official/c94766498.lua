032644f57d07ae9a765e3bb91fa469ac5c4fbf2c
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-25 14:28:57 +0000

    "Chronomaly Aztec Mask Golem" fix
    Should not be able to Special Summon it if you activated a "Chronomaly" Trap Card earlier that turn.

---
+++ script/official/c94766498.lua
@@ -0,0 +1,24 @@
+--先史遺産アステカ・マスク・ゴーレム
+--Chronomaly Aztec Mask Golem
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	--Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.hspcon)
+	c:RegisterEffect(e1)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,s.chainfilter)
+end
+s.listed_series={0x70}
+function s.chainfilter(re,tp,cid)
+	return not (re:IsActiveType(TYPE_SPELL) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsSetCard(0x70))
+end
+function s.hspcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetCustomActivityCount(id,tp,ACTIVITY_CHAIN)>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
