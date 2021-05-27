345982488719c14cc5787628a925a48a54fedd72
Larry126 <28985622+larry126@users.noreply.github.com>
2021-05-27 03:52:24 +0000

    Unofficial anime script fixes
    - Chiron the Mage (Anime) updated in line with the anime proxy text
    - Number 3: Cicada King is properly counted as a "Cicada " card

---
+++ script/unofficial/c511310000.lua
@@ -0,0 +1,40 @@
+--賢者ケイローン (Anime)
+--Chiron the Mage (Anime)
+--Scripted by AlphaKretin
+--Re-scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id)==0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:GetControler()~=tp and chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsFacedown() end
+	if chk==0 then return true end
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	local g=Duel.SelectTarget(tp,Card.IsFacedown,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if #g==0 then return end
+	local ac=Duel.AnnounceCard(tp)
+	Duel.SetTargetParam(ac)
+	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not (tc and tc:IsRelateToEffect(e) and tc:IsFacedown()) then return end
+	local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	Duel.ConfirmCards(tp,tc)
+	if tc:IsCode(ac) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
