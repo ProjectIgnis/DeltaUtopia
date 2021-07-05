044b10511f41a8e868cb5a1baafee80336ecd0ea
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-07-05 08:11:28 +0000

    "Farm Delivery" fix
    Fixed wrong function name, also, can target up to 3, not exactly 3, plus other script polishing.

---
+++ script/official/c27001740.lua
@@ -0,0 +1,34 @@
+--農園からの配送
+--Farm Delivery
+--Scripted by AlphaKretin
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCondition(s.tdcon)
+	e1:SetTarget(s.tdtg)
+	e1:SetOperation(s.tdop)
+	c:RegisterEffect(e1)
+end
+function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_NORMAL),tp,LOCATION_MZONE,0,1,nil)
+end
+function s.tdfilter(c)
+	return c:IsType(TYPE_NORMAL) and c:IsFaceup() and c:IsAbleToDeck()
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and s.tdfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local tg=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_REMOVED,0,1,3,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,tg,#tg,0,0)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	if #tg==0 then return end
+	Duel.SendtoDeck(tg,tp,SEQ_DECKSHUFFLE,REASON_EFFECT)
+end
