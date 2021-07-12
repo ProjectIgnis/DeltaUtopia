d553832e6977e3f4e33cd5c5c1b54a3c2f26df84
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-07-12 20:11:37 +0000

    "Spell Canceller" fix
    Fixed an issue where it would prevent the effects of Spells in the GY from being activated.

---
+++ script/official/c84636823.lua
@@ -0,0 +1,38 @@
+--マジック・キャンセラー
+--Spell Canceller
+local s,id=GetID()
+function s.initial_effect(c)
+	--cannot trigger
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(1,1)
+	e1:SetValue(s.aclimit)
+	c:RegisterEffect(e1)
+	--disable
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DISABLE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_SZONE,LOCATION_SZONE)
+	e2:SetTarget(s.distg)
+	c:RegisterEffect(e2)
+	--disable effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_CHAIN_SOLVING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetOperation(s.disop)
+	c:RegisterEffect(e3)
+end
+function s.aclimit(e,re,tp)
+	return re:IsActiveType(TYPE_SPELL) and (re:GetHandler():IsOnField() or re:IsHasType(EFFECT_TYPE_ACTIVATE))
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local tl=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
+	if (tl&LOCATION_SZONE)~=0 and re:IsActiveType(TYPE_SPELL) then
+		Duel.NegateEffect(ev)
+	end
+end
