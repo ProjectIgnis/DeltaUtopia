7ebb7570fa807b1a2a2d7af87e34c8fd7e5a5c00
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-07-19 19:00:03 +0000

    "Zefra Metaltron" fix
    Its first effect should be optional. Also paritially rewrote the effect to simplify it and fix some edge cases.

---
+++ script/official/c85216896.lua
@@ -0,0 +1,126 @@
+--セフィラ・メタトロン
+--Zefra Metaltron
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,s.matfilter,2)
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_LEAVE_FIELD_P)
+	e1:SetCondition(s.regcon)
+	e1:SetOperation(s.regop)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.thcon)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+	--banish
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id+1)
+	e3:SetTarget(s.rmtg)
+	e3:SetOperation(s.rmop)
+	c:RegisterEffect(e3)
+end
+function s.matfilter(c)
+	return c:IsSummonLocation(LOCATION_EXTRA)
+end
+function s.thcfilter(c,lc,tp)
+	return (c:IsReason(REASON_BATTLE) or (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp))
+		and lc:GetLinkedGroup():IsContains(c) and c:IsSummonLocation(LOCATION_EXTRA)
+		and c:IsLocation(LOCATION_MZONE)
+end
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return eg:IsExists(s.thcfilter,1,c,c,tp)
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_CHAIN,0,1)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id)>0
+end
+function s.thfilter(c)
+	return (not c:IsLocation(LOCATION_EXTRA) or (c:IsFaceup() and c:IsType(TYPE_PENDULUM)))
+		and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE+LOCATION_EXTRA)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g,true)
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.rmfilter(c)
+	return c:IsFaceup() and c:IsSummonLocation(LOCATION_EXTRA) and c:IsAbleToRemove()
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.rmfilter,tp,LOCATION_MZONE,0,1,e:GetHandler())
+		and Duel.IsExistingTarget(s.rmfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g1=Duel.SelectTarget(tp,s.rmfilter,tp,LOCATION_MZONE,0,1,1,e:GetHandler())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g2=Duel.SelectTarget(tp,s.rmfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	g1:Merge(g2)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g1,2,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	for tc in aux.Next(g) do
+		if Duel.Remove(tc,0,REASON_EFFECT+REASON_TEMPORARY)~=0 then
+			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+		end
+	end
+	g:KeepAlive()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetLabelObject(g)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.retcon)
+	e1:SetOperation(s.retop)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.retfilter(c)
+	return c:GetFlagEffect(id)~=0
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	if not g:IsExists(s.retfilter,1,nil) then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return true end
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject():Filter(s.retfilter,nil)
+	g:DeleteGroup()
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		Duel.ReturnToField(tc)
+	end
+end
