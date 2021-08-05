71a1d434315453a12bb0b583e1a24d84c22e3a9b
Edoardo Lolletti <edoardo762@gmail.com>
2021-08-05 16:04:48 +0000

    Fix Speed Spell - Takeover
    Wasn't properly gaining lp based on the counters on the destroyed card

---
+++ script/unofficial/c511000978.lua
@@ -0,0 +1,45 @@
+--Ｓｐ－テイク・オーバー
+--Speed Spell - Takeover
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCondition(s.con)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFieldCard(tp,LOCATION_FZONE,0)
+	return tc and tc:GetCounter(0x91)>3
+end
+function s.filter(c)
+	return c:IsFaceup() and c:HasCounters() and c:IsDestructable()
+end
+function s.countcounters(c)
+    local count=0
+    for counter,amount in pairs(c:GetAllCounters()) do
+        count=count+amount
+    end
+	return count
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,s.countcounters(g:GetFirst())*100)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) then return end
+	local lp=s.countcounters(tc)*100
+    if Duel.Destroy(tc,REASON_EFFECT)>0 then
+        Duel.Recover(tp,lp,REASON_EFFECT)
+    end
+end
