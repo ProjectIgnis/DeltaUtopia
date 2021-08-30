8e00f75ea1819cc2c2ddb020bf5db24914bf48f8
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-08-30 03:29:19 +0000

    Fix "Bacon Saver (Anime)"
    Changed code to better reflect text and updated attack handling to prevent script errors.

---
+++ script/unofficial/c511013031.lua
@@ -0,0 +1,52 @@
+--タスケルトン (Anime)
+--Bacon Saver (Anime)
+--fixed by MLD
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(95360850,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_BATTLE_START)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetHintTiming(TIMING_BATTLE_PHASE)
+	e1:SetCost(aux.bfgcost)
+	e1:SetCondition(s.atkcon)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if a:IsControler(tp) and a:IsRelateToBattle() then
+		e:SetLabelObject(a)
+		return true
+	end
+	if d:IsControler(tp) and d:IsRelateToBattle() then
+		e:SetLabelObject(d)
+		return true
+	end
+	return false
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=e:GetLabelObject()
+	if chk==0 then return true end
+	Duel.SetTargetCard(tc)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+		e2:SetValue(1)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
+		tc:RegisterEffect(e2)
+	end
+end
