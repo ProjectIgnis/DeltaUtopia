a495b4ba88a8be80b9e5a0ef0dbe189cee559b3d
Hatter <sanity.sierota@protonmail.com>
2021-10-09 09:25:10 +0000

    Update "Maiden of the Aqua"
    add "Umi" as a listed card

---
+++ script/official/c17214465.lua
@@ -0,0 +1,12 @@
+--海神の巫女
+local s,id=GetID()
+function s.initial_effect(c)
+	--field
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_CHANGE_ENVIRONMENT)
+	e1:SetValue(CARD_UMI)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_UMI}
