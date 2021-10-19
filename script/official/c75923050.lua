bb6628792eb3d2b61cdebef944bf7cab9216f6ed
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-10-19 13:40:07 +0000

    "Super Roboyarou" & "Super Robolady" updates
    "Super Roboyarou" should gain ATK even it's attacked, not only when it attacks. Also, general script polishing for both.

---
+++ script/official/c75923050.lua
@@ -0,0 +1,57 @@
+--レアメタル・ヴァルキリー
+--Super Robolady
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,92421852,38916461)
+	--Increase ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.atkcon)
+	e1:SetValue(1000)
+	c:RegisterEffect(e1)
+	--Special Summon "Super Roboyarou"
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={1412158}
+function s.atkcon(e)
+	local ph=Duel.GetCurrentPhase()
+	if not (ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL) then return false end
+	return Duel.GetAttacker()==e:GetHandler() and not Duel.GetAttackTarget()
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetTurnID()~=Duel.GetTurnCount()
+end
+function s.spfilter(c,e,tp,mc)
+	return c:IsCode(1412158) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:GetOwner()==tp and c:IsAbleToExtra()
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not (c:IsRelateToEffect(e) and c:IsFaceup()) then return end
+	if Duel.SendtoDeck(c,nil,2,REASON_EFFECT)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,c)
+		if #sg==0 then return end
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
