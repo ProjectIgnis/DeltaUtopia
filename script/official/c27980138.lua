85cd6a15fb5b59632db67b36fd404fde24bb6574
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-11-01 18:50:58 +0000

    "Quill Pen of Gulldos" update
    Script polishing.

---
+++ script/official/c27980138.lua
@@ -0,0 +1,40 @@
+--ガルドスの羽根ペン
+--Quill Pen of Gulldos
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.tdfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsMonster() and c:IsAbleToDeck()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE,0,2,nil)
+		and Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g1=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g2=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g1,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g2,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	if #tg<2 then return end
+	local dg=tg:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
+	if #dg~=2 then return end
+	local hg=tg:Filter(Card.IsLocation,nil,LOCATION_ONFIELD)
+	if Duel.SendtoDeck(dg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0
+		and dg:IsExists(Card.IsLocation,2,nil,LOCATION_DECK+LOCATION_EXTRA)
+		and #hg>0 then
+		Duel.SendtoHand(hg,nil,REASON_EFFECT)
+	end
+end
