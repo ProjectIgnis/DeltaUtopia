9fd2778c4a47dcb9669afe9bc0a0d5e5b2fc4901
Larry126 <28985622+larry126@users.noreply.github.com>
2021-11-16 08:50:12 +0000

    Unofficial script fixes
    - Skiel Attack 3 atk change effect fixed with final
    - Fancy Footwork GY effect fixed
    - Toon Briefcase (Anime) effect corrected
    - Galaxy Rebirth's halve atk effect fixed
    - Power Bond (anime) fixed
    - D/D Brownie now summon in attack pos
    - Prime Photon's atk gain effect fixed
    - Odd-Eyes Lancer Dragon (Anime) no longer misses timing
    - Commander Covington (Manga) no longer misses timing
    - Exslayer's tribute cost modernised
    - Number C80's reset typo fixed

---
+++ script/unofficial/c511000629.lua
@@ -0,0 +1,33 @@
+--トゥーンのかばん (Anime)
+--Toon Briefcase (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={15259703}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,15259703),tp,LOCATION_ONFIELD,0,1,nil)
+end
+function s.filter(c,p)
+	return c:IsSummonPlayer(p) and c:IsAbleToDeck() and c:IsLocation(LOCATION_MZONE)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(s.filter,1,nil,1-tp) end
+	local g=eg:Filter(s.filter,nil,1-tp)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e):Filter(Card.IsRelateToEffect,nil,e)
+	if #g>0 then
+		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
