176d3c6eefad3df329757a29abda1529c3ade8a5
Larry126 <28985622+larry126@users.noreply.github.com>
2022-01-02 14:34:58 +0000

    Unofficial script updates
    - Regen Warrior effect update
    - Promotion id update

---
+++ script/unofficial/c511009143.lua
@@ -0,0 +1,37 @@
+--プロモーション
+--Promotion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={77590412,15001619,511009178,511009179,89839552}
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsCode,1,false,nil,nil,89839552) end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsCode,1,1,false,nil,nil,89839552)
+	Duel.Release(g,REASON_COST)
+end
+function s.filter(c,e,tp)
+	return (c:IsCode(77590412) or c:IsCode(15001619) or c:IsCode(511009178) or c:IsCode(511009179)) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)
+		g:GetFirst():CompleteProcedure()
+	end
+end
