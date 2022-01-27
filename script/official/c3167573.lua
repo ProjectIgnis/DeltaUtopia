ab237fbe10531ed750c9f3934c603892c9ea552b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-01-27 23:35:20 +0000

    "Puppet King" + "Puppet Queen" fix
    Added a new custom event so that their effects cannot be activated when a monster is added to the hand by an effect that doesn't reveal the card (e.g. "Bingo Machine, Go!!!"). In other words, the game state from the player's perspective doesn't know that the card added to the hand is a monster. Also, general script polish for "Puppet King" & "Puppet Queen".

---
+++ script/official/c3167573.lua
@@ -0,0 +1,57 @@
+--パペット・キング
+--Puppet King
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_TOHAND_CONFIRM)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.descon)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+	e1:SetLabelObject(e2)
+end
+function s.cfilter(c,tp)
+	return c:IsControler(1-tp) and c:IsPreviousLocation(LOCATION_DECK) and not c:IsReason(REASON_DRAW)
+		and c:IsMonster() and c:GetReasonPlayer()==1-tp
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local des_eff=e:GetLabelObject()
+		local ct=Duel.IsTurnPlayer(tp) and 2 or 1
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,ct)
+		des_eff:SetLabel(Duel.GetTurnCount()+ct)
+	end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id)>0 and Duel.GetTurnCount()==e:GetLabel()
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	e:SetLabel(0)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+end
