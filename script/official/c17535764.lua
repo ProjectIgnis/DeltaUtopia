ef984018325b7d3bcf86d1e425fe81f01b4bdcd8
Naim <naimsantos45@gmail.com>
2022-02-25 14:07:22 +0000

    "Gokipole" fix
    Missing effect categories + general script polish.

---
+++ script/official/c17535764.lua
@@ -0,0 +1,47 @@
+--ゴキポール
+--Gokipole
+--Scripted by AlphaKretin
+local s,id=GetID()
+function s.initial_effect(c)
+	--Search 1 Level 4 Insect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+end
+function s.thfilter(c)
+	return c:IsLevel(4) and c:IsRace(RACE_INSECT) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g==0 or Duel.SendtoHand(g,nil,REASON_EFFECT)==0 then return end
+	local tc=g:GetFirst()
+	Duel.ConfirmCards(1-tp,tc)
+	if tc:IsType(TYPE_NORMAL) and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.BreakEffect()
+		if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)==0 then return end
+		local dg=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsAttackAbove,tc:GetAttack()),tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+		if #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+			local dc=dg:Select(tp,1,1,nil)
+			if #dc>0 then
+				Duel.BreakEffect()
+				Duel.HintSelection(dc,true)
+				Duel.Destroy(dc,REASON_EFFECT)
+			end
+		end
+	end
+end
+--They see me rollin'
+--They hatin'
