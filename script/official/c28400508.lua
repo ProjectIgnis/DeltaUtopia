bf8b04f4f461e6863697c4285162b86605cd508f
Naim <naimsantos45@gmail.com>
2022-03-06 13:26:08 +0000

    "Number 97: Draglubion" fix
    Was not specifying where it could possibly Special Summon from.

---
+++ script/official/c28400508.lua
@@ -0,0 +1,84 @@
+--龍影神ドラッグラビオン
+--Number 97: Draglubion
+--Scripted by AlphaKretin
+local s,id=GetID()
+function s.initial_effect(c)
+	--Xyz Summon procedure
+	Xyz.AddProcedure(c,nil,8,2)
+	c:EnableReviveLimit()
+	--Cannot be targeted by the opponent's effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(aux.tgoval)
+	c:RegisterEffect(e1)
+	--Special Summon 1 "Number" Dragon monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(aux.dxmcostgen(1,1,nil))
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2,false,REGISTER_FLAG_DETACH_XMAT)
+end
+s.listed_series={0x48}
+s.listed_names={id}
+s.xyz_number=97
+function s.spfilter(c)
+	return c:IsRace(RACE_DRAGON) and c:IsSetCard(0x48) and not c:IsCode(id)
+end
+function s.spchk(c,e,tp)
+	if not c:IsCanBeSpecialSummoned(e,0,tp,false,false) then return false end
+	if c:IsLocation(LOCATION_EXTRA) then
+		return Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+	end
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:IsExists(s.spchk,1,nil,e,tp) and sg:GetClassCount(Card.GetCode)==#sg
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA+LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler() 
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,nil)
+	if aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) then
+		local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_SELECT)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local tg=sg:Filter(s.spchk,nil,e,tp):Select(tp,1,1,nil)
+		if Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_FACEUP)~=0 then
+			local oc=sg-tg
+			local tc=tg:GetFirst()
+			Duel.Overlay(tc,oc)
+			--Limit attacks for the rest of the turn
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetCode(EFFECT_CANNOT_ATTACK)
+			e1:SetTargetRange(LOCATION_MZONE,0)
+			e1:SetLabelObject(tc)
+			e1:SetTarget(s.atktg)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			Duel.RegisterEffect(e1,tp)
+		end
+	end
+	--Cannot Special Summon other monsters
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e2:SetTargetRange(1,0)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.atktg(e,c)
+	return e:GetLabelObject()~=c
+end
