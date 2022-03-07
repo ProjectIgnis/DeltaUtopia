05870a9376aca5ec289f40f5a4256bd5fe243276
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-03-07 22:56:05 +0000

    Various fixes
    - Dinomist Rush: The summoned monster shouldn't be unaffected by its own effects + general polish.
    - Duel Academy: Shouldn't be able to use any of its effects if this card's effects are currently negated.
    - Eldlich the Golden Lord: Shuffle the hand after returning it to the hand + general polish.
    - Hand Destruction: Shouldn't draw if no cards end up in the GY.
    - Last Counter: Fixed the case where if the opponent declared the attack, the damage calculation and effect damage wouldn't occur + general polish.
    - Lyrilusc - Phantom Feathers: Should be able to activate it in the Damage Step.
    - Magicians' Combination: Removed old Continuous Trap handling + general polish.
    - Mysterious Guard: Rewrote the script to match its most recent OCG print and rulings.
    - Reptia Egg: The flag effect that was registered on Normal Summon was overlapping with the flag effect used for the banishing part of the effect, causing issues when Special Summoning another "Reptia Egg".
    - Scanner: Shouldn't be able to target a Link Monster + general polish.

---
+++ script/official/c86049351.lua
@@ -0,0 +1,60 @@
+--ラスト・カウンター
+--Last Counter
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x84}
+function s.cfilter(c,pos)
+	return c:IsPosition(pos) and c:IsSetCard(0x84)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local bc1,bc2=Duel.GetBattleMonster(tp)
+	if not (bc1 and bc1:IsFaceup() and bc1:IsSetCard(0x84)) then return false end
+	if not (bc2 and bc2:IsFaceup()) then return false end
+	local pos=POS_FACEUP
+	if bc1==Duel.GetAttacker() then pos=POS_FACEUP_ATTACK end
+	e:SetLabelObject(bc1)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,bc1,pos)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,e:GetLabelObject():GetBattleTarget():GetBaseAttack())
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if not (Duel.NegateAttack() and tc:IsRelateToBattle()) then return end
+	if tc:IsControler(tp) and Duel.SendtoGrave(tc,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_GRAVE) then
+		local bc=tc:GetBattleTarget()
+		if not (bc:IsRelateToBattle() and bc:IsFaceup() and bc:IsControler(1-tp)) then return end
+		local pos=POS_FACEUP
+		if tc==Duel.GetAttacker() then pos=POS_FACEUP_ATTACK end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+		local sc=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil,pos):GetFirst()
+		if not sc then return end
+		Duel.HintSelection(sc,true)
+		local atk=bc:GetBaseAttack()
+		if atk<0 then atk=0 end
+		--Increase ATK
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		sc:RegisterEffect(e1)
+		if bc==Duel.GetAttackTarget() then bc,sc=sc,bc end
+		if sc:IsCanBeBattleTarget(bc) and not bc:IsImmuneToEffect(e) and not sc:IsImmuneToEffect(e) then
+			Duel.CalculateDamage(bc,sc)
+			Duel.BreakEffect()
+			Duel.Damage(tp,atk,REASON_EFFECT)
+		end
+	end
+end
---
+++ script/official/c86509711.lua
@@ -0,0 +1,82 @@
+--マジシャンズ・コンビネーション
+--Magicians' Combination
+--Scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Negate that effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.negcon)
+	e2:SetCost(s.negcost)
+	e2:SetTarget(s.negtg)
+	e2:SetOperation(s.negop)
+	c:RegisterEffect(e2)
+	--Destroy 1 card on the field
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCondition(s.descon)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={CARD_DARK_MAGICIAN,CARD_DARK_MAGICIAN_GIRL}
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_DESTROY_CONFIRMED) and Duel.IsChainDisablable(ev)
+end
+function s.cfilter(c,e,tp)
+	return c:IsCode(CARD_DARK_MAGICIAN,CARD_DARK_MAGICIAN_GIRL) and Duel.GetMZoneCount(tp,c)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp,c:GetCode())
+end
+function s.spfilter(c,e,tp,code)
+	return c:IsCode(CARD_DARK_MAGICIAN,CARD_DARK_MAGICIAN_GIRL) and not c:IsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,e,tp) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,e,tp)
+	e:SetLabel(g:GetFirst():GetCode())
+	Duel.Release(g,REASON_COST)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local code=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp,code)
+	if #sg>0 and Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)>0 then
+		Duel.NegateEffect(ev)
+	end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_SZONE) and c:IsPreviousPosition(POS_FACEUP)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local dg=Duel.GetFieldGroup(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)
+	if chk==0 then return #dg>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local dg=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	if #dg>0 then
+		Duel.Destroy(dg,REASON_EFFECT)
+	end
+end
