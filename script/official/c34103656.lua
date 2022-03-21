3cb714e831b5648b9bac588c169dc98cac49259c
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-03-21 16:10:19 +0000

    Various fixes
    - Accel Synchro Stardust Dragon: Fixed its interaction with "Cosmic Flare". If "Cosmic Flare" is banished instead then this card should be considered as a valid Synchro Material.
    - Amazoness Village: Shouldn't trigger if an "Amazoness" is destroyed by game mechanics + general script polish.
    - Dark Spell Regeneration: Added strings.
    - D/D/D Rebel King Leonidas: Added strings.
    - Fortune Lady Dark: Should check that the destroyed monster was on the opponent's field.
    - Lemuria, the Forgotten City: Prevent a script error if the player controls no face-up WATER monsters when its effect resolves.
    - Raviel, Lord of Phantasms: Shouldn't be able to Tribute a monster with 0 original ATK for its effect.
    - Sky Striker Ace - Raye: Missing the "Special Summon" category on her GY effect, also, that effect shouldn't trigger if sent to the GY together with a "Sky Striker Ace" Link + general script polish.
    - Thunder Hand: Should also check that the card sent to the GY was destroyed + general script polish.

---
+++ script/official/c34103656.lua
@@ -0,0 +1,59 @@
+--忘却の都 レミューリア
+--Lemuria, the Forgotten City
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Increase ATK/DEF
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_WATER))
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetValue(200)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+	--Increase Level
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_LVCHANGE)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCountLimit(1)
+	e4:SetTarget(s.lvtg)
+	e4:SetOperation(s.lvop)
+	c:RegisterEffect(e4)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER) and c:HasLevel()
+end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local lv=Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsAttribute,ATTRIBUTE_WATER),tp,LOCATION_MZONE,0,nil)
+	if lv==0 then return end
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	local _,fid=g:GetMaxGroup(Card.GetFieldID)
+	local c=e:GetHandler()
+	--Increase Level
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(s.efftg)
+	e1:SetValue(lv)
+	e1:SetLabel(fid)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
+end
+function s.efftg(e,c)
+	return c:GetFieldID()<=e:GetLabel() and c:IsAttribute(ATTRIBUTE_WATER) and c:HasLevel()
+end
