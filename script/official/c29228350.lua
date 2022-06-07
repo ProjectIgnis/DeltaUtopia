1466956b50097b432fc4d94a45a198f6fc2296be
Edoardo Lolletti <edoardo762@gmail.com>
2022-06-07 11:57:48 +0000

    Update Dragon Laser
    Should be able to target "Trigon" on the opponent's field as long as they're equipped to a "Delta Tri" on your field.

---
+++ script/official/c29228350.lua
@@ -0,0 +1,38 @@
+--ドラゴンレーザー
+--Dragon Laser
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={48568432,12079734}
+function s.filter(c,tp)
+	local ec=c:GetEquipTarget()
+	return ec and ec:IsControler(tp) and c:IsCode(48568432) and ec:IsCode(12079734)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_SZONE) and s.filter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_SZONE,LOCATION_SZONE,1,nil,tp)
+		and Duel.GetFieldGroupCount(1-tp,LOCATION_MZONE,0)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_SZONE,LOCATION_SZONE,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+	local sg=Duel.GetFieldGroup(1-tp,LOCATION_MZONE,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoGrave(tc,REASON_EFFECT)
+	end
+	local sg=Duel.GetFieldGroup(1-tp,LOCATION_MZONE,0)
+	Duel.Destroy(sg,REASON_EFFECT)
+end
