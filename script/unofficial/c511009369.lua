ae3a0649c8d2b075acb57076706a3858c36f131e
Naim <naimsantos45@gmail.com>
2022-06-12 21:45:12 +0000

    Update "Timebreaker Magician (Anime)"
    Removed an incorrect flag used in the monster effect

---
+++ script/unofficial/c511009369.lua
@@ -0,0 +1,90 @@
+--刻剣の魔術師
+--Timebreaker Magician (Anime)
+--fixed by MLD
+local s,id=GetID()
+function s.initial_effect(c)
+	--Pendulum Summon
+	Pendulum.AddProcedure(c)
+	--Double its own ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.atkcon)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+	--Cannot attack directly
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	c:RegisterEffect(e2)
+	--Double a pendulum scale
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e3:SetCode(511002005)
+	e3:SetRange(LOCATION_PZONE)
+	e3:SetTarget(s.sctg)
+	e3:SetOperation(s.scop)
+	c:RegisterEffect(e3)
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ADJUST)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return #eg==1 and eg:GetFirst()==c
+		and c:IsSummonType(SUMMON_TYPE_PENDULUM) and c:IsPreviousLocation(LOCATION_HAND)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(c:GetAttack()*2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+	end
+end
+function s.cfilter(c)
+	local seq=c:GetSequence()
+	return c:GetFlagEffect(511002005+seq)==0 and (not c:IsPreviousLocation(LOCATION_PZONE) or c:GetPreviousSequence()~=seq)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local tot=Duel.IsDuelType(DUEL_SEPARATE_PZONE) and 13 or 4
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_PZONE,LOCATION_PZONE,nil)
+	if #g>0 then
+		for tc in g:Iter() do
+			tc:ResetFlagEffect(511002005+tot-tc:GetSequence())
+			Duel.RaiseSingleEvent(tc,511002005,e,0,tp,tp,0)
+			tc:RegisterFlagEffect(511002005+tc:GetSequence(),RESET_EVENT+RESETS_STANDARD,0,1)
+		end
+	end
+end
+function s.sctg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_PZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_PZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_PZONE,0,1,1,nil)
+end
+function s.scop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LSCALE)
+		e1:SetValue(tc:GetLeftScale()*2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_CHANGE_RSCALE)
+		e2:SetValue(tc:GetRightScale()*2)
+		tc:RegisterEffect(e2)
+	end
+end
