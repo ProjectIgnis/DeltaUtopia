187a1aed1d2c5cdee8b88437c45af3ce28e271ac
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-07-05 11:34:10 +0000

    Some fixes
    Official/Pre-release:
    - Dark Rebellion Xyz Dragon: Missing ATK change category + general script polish.
    - Fuhma Wave: Should work with monsters that have multiple Attributes. Also, effects like "Stardust Dragon" can now be chained to it, and if the targeted monster isn't face-up and related to the effect anymore it shouldn't destroy.
    - Legend of Heart: Inaccurate operation info + general script polish.
    
    Anime:
    - Dark Rebellion Xyz Dragon (Anime): Missing ATK change category + general script polish.
    - Legend of Heart (Anime): Inaccurate operation info.

--- script/official/c16195942.lua
+++ script/official/c16195942.lua
@@ -1,5 +1,5 @@
---ダーク・リベリオン・エクシーズ・ドラゴン (Anime)
---Dark Rebellion Xyz Dragon (Anime)
+--ダーク・リベリオン・エクシーズ・ドラゴン
+--Dark Rebellion Xyz Dragon
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
@@ -12,19 +12,16 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCost(aux.dxmcostgen(1,1,nil))
+	e1:SetCost(aux.dxmcostgen(2,2,nil))
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
 end
-function s.atkfilter(c)
-	return c:IsLevelAbove(5) and aux.nzatk(c)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.atkfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.atkfilter,tp,0,LOCATION_MZONE,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and aux.nzatk(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(aux.nzatk,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.atkfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,aux.nzatk,tp,0,LOCATION_MZONE,1,1,nil)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
@@ -35,7 +32,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e1:SetValue(val)
 		tc:RegisterEffect(e1)
 		if c:IsRelateToEffect(e) and c:IsFaceup() then
@@ -44,7 +41,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetCode(EFFECT_UPDATE_ATTACK)
 			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 			e2:SetValue(val)
 			c:RegisterEffect(e2)
 		end
