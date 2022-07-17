56426a06984bc53fd2b2f9bb12b27b8f88462178
Naim <naimsantos45@gmail.com>
2022-07-17 11:36:29 +0000

    Update cards that draw and return cards to the Deck
    The hand should be shuffled, to prevent the opponent from knowing if the same card was returned to the deck

---
+++ script/official/c47025270.lua
@@ -0,0 +1,29 @@
+--白兵戦型お手伝いロボ
+--Helping Robo for Combat
+local s,id=GetID()
+function s.initial_effect(c)
+	--Draw 1 card and return 1 card to the Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_BATTLE_DESTROYING)
+	e1:SetTarget(s.drtg)
+	e1:SetOperation(s.drop)
+	c:RegisterEffect(e1)
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.Draw(tp,1,REASON_EFFECT)>0 then
+		Duel.ShuffleHand(tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_HAND,0,1,1,nil)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.SendtoDeck(g,nil,1,REASON_EFFECT)
+		end
+	end
+end
