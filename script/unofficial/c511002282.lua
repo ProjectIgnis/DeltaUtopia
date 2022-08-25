79622b9ac2796fe0230baf7324394793a8c67f21
Larry126 <28985622+larry126@users.noreply.github.com>
2022-08-25 14:28:28 +0000

    Update anime scripts
    Updated anime archetypes:
    - Hell
    - Cenozoic/Mesozoic/Paleozoic
    Updated Black Mamba (Manga) & Promotion (Manga)
    - Minor cleanup

---
+++ script/unofficial/c511002282.lua
@@ -0,0 +1,26 @@
+--ブラックマンバ (Manga)
+--Black Mamba (Manga)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Change position
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.postg)
+	e1:SetOperation(s.posop)
+	c:RegisterEffect(e1)
+end
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,1-tp,LOCATION_MZONE)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsCanChangePosition,tp,0,LOCATION_MZONE,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g)
+		Duel.ChangePosition(g,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
+	end
+end
