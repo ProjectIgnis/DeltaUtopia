2bb22c37e2fd4beb576db027a38a4ba07b3f1cf0
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-08-27 16:28:24 +0000

    Various script fixes/updates
    Official:
    - Alligator’s Sword Dragon: Simplified and cleaned up the effect's condition.
    - Drytron Meteonis Draconids: Added a hint on it for when the total Levels of monsters used for its Ritual Summon were 2 or less.
    - Drytron Meteonis Quadrantids: Added a hint on it for when the total Levels of monsters used for its Ritual Summon were 2 or less.
    - Gravity Balance: Added checks to enforce the "both" part of its effect.
    - Rose Princess: Doesn't mention the "Rose" archetype in its text + general polish.
    - Ursarctic Drytron: Should include the possibility of banishing from the Deck.
    
    Rush:
    - Alligator’s Sword Dragon (Rush): Same as "Alligator’s Sword Dragon" above + it shouldn't check that the player can send the top card of their Deck to the GY.
    - Buster Blader (Rush): Now accounts for Maximum monsters + small polish.
    - Earth Pressure Explosion: Should check the destroyed monster's Type on the field.
    
    Unofficial:
    - Greed Quasar (Manga): Explicitly return "false" if a condition check fails.

---
+++ script/official/c3366982.lua
@@ -0,0 +1,20 @@
+--ドラゴンに乗るワイバーン
+--Alligator’s Sword Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion Material
+	Fusion.AddProcMix(c,true,true,88819587,64428736)
+	--Can attack directly
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DIRECT_ATTACK)
+	e2:SetCondition(s.dircon)
+	c:RegisterEffect(e2)
+end
+function s.dircon(e)
+	local ATTRIBUTE_EARTH_WATER_FIRE=ATTRIBUTE_EARTH|ATTRIBUTE_WATER|ATTRIBUTE_FIRE
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,e:GetHandlerPlayer(),0,LOCATION_MZONE,nil)
+	local ct=#g
+	return ct>0 and g:FilterCount(Card.IsAttribute,nil,ATTRIBUTE_EARTH_WATER_FIRE)==ct
+end
