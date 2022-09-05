7254cbe6bffabb93cdfbf22791eb1360e80515a0
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2022-09-05 16:08:41 +0000

    Update "Adapt to Adversity"
    Should be a continuous effect that protects all controller's Normal or Special Summoned WATER monsters

---
+++ script/unofficial/c511001449.lua
@@ -0,0 +1,25 @@
+--逆境適応
+--Adapt to Adversity
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	 --Prevent destruction by opponent's effect for your Normal/Special Summoned WATER monsters
+    	local e2=Effect.CreateEffect(c)
+    	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    	e2:SetRange(LOCATION_SZONE)
+    	e2:SetTargetRange(LOCATION_MZONE,0)
+    	e2:SetTarget(s.indtg)
+    	e2:SetValue(s.indesval)
+    	c:RegisterEffect(e2)
+end
+function s.indtg(e,c)
+    return c:IsMonster() and c:IsAttribute(ATTRIBUTE_WATER) and (c:IsSummonType(SUMMON_TYPE_NORMAL) or c:IsSummonType(SUMMON_TYPE_SPECIAL))
+end
+function s.indesval(e,re)
+	return re:IsActiveType(TYPE_SPELL+TYPE_TRAP)
+end
